ad4e5f5bbae4c85a432a5a885e248774d0c709f9
Hiseous <hiseous@gmail.com>
2024-11-27 09:16:06 +0100

    inventory

--- hooks/useFile.ts
+++ hooks/useFile.ts
@@ -1,29 +1,44 @@
 
-export const convertImageToBase64 = async (uri: string) => {
-    console.log('1');
-    let base64String: string | undefined;
-console.log(uri)
-    if(uri){
-        try {
-        const response = await fetch(uri);
-        const blob = await response.blob();
-    
-        const reader = new FileReader();
-        reader.onloadend = () => {
-            console.log('2');
-            const result = reader.result ?? undefined;
-            if(typeof result === 'string'){
-                base64String = result;
-                // base64String = result.split(',')[1]; //to remove the prefix
-                console.log('Base64 String:', base64String);
+type promiseResponse = {
+    base64String?: string;
+    error?: string;
+}
+type promiseCallback = (promiseResponse: promiseResponse) => void;
+const promiseToConvertImageToBase64 = (uri: string) => {
+    return new Promise(async (resolve: promiseCallback, reject: promiseCallback) => {
+        if(uri){
+            try {
+            const response = await fetch(uri);
+            const blob = await response.blob();
+        
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                const result = reader.result ?? undefined;
+                if(typeof result === 'string'){
+                    resolve({
+                        base64String: result,
+                    });
+                    // base64String = result.split(',')[1]; //to remove the prefix
+                    console.log('Base64 String:', result);
+                }
+                else {
+                    reject({
+                        error: `could not convert image`,
+                    });
+                }
+            };
+            reader.readAsDataURL(blob);
+            } catch (error) {
+                console.error(error);
+                reject({
+                    error: `error converting image to Base64`,
+                });
             }
-        };
-        reader.readAsDataURL(blob);
-        } catch (error) {
-        console.error('Error converting image to Base64:', error);
         }
-    }
-    console.log('3');
+    });
+};
+export const convertImageToBase64 = async (uri: string) => {
+    const base64String = (await promiseToConvertImageToBase64(uri)).base64String;
 
     return {
         base64String,
